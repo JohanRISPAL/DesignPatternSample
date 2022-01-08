eabca713349043d4f10fae652ad8fcaefbba4fd7
Johan <johanrispalgit@gmail.com>
2022-01-08 20:20:46 +0100

    Weapon's sub composite finish

---
+++ Composite/Composite/BluntWeapon.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.Composite
+{
+    internal class BluntWeapon : IInventoryItem
+    {
+        private List<IInventoryItem> _bluntWeapons;
+
+        public BluntWeapon()
+        {
+            _bluntWeapons = new List<IInventoryItem>();
+        }
+        public void DisplayName()
+        {
+            foreach (var bluntWeapon in _bluntWeapons)
+            {
+                bluntWeapon.DisplayName();
+            }
+        }
+
+        public int GetWeight()
+        {
+            int totalBluntWeaponWeight = 0;
+
+            foreach (var bluntWeapon in _bluntWeapons)
+            {
+                totalBluntWeaponWeight += bluntWeapon.GetWeight();
+            }
+
+            return totalBluntWeaponWeight;
+        }
+
+        public int GetPotionSpace()
+        {
+            int totalBluntWeaponPotionSpace = 0;
+
+            foreach (var bluntWeapon in _bluntWeapons)
+            {
+                totalBluntWeaponPotionSpace += bluntWeapon.GetPotionSpace();
+            }
+
+            return totalBluntWeaponPotionSpace;
+        }
+
+        internal void AddBluntWeapon(IInventoryItem bluntWeaponToAdd)
+        {
+            _bluntWeapons.Add(bluntWeaponToAdd);
+        }
+
+        internal void RemoveBluntWeapon(IInventoryItem bluntWeaponToDelete)
+        {
+            _bluntWeapons.Remove(bluntWeaponToDelete);
+        }
+
+        public void Load(List<IInventoryItem> itemsList)
+        {
+            foreach (var component in _bluntWeapons)
+            {
+                component.Load(itemsList);
+            }
+        }
+    }
+}
--- Composite/Composite/Weapon.cs
+++ Composite/Composite/Weapon.cs
@@ -13,11 +13,13 @@ namespace Composite
         private List<IInventoryItem> _weapons;
 
         private BladeWeapon _bladeWeapons = new BladeWeapon();
+        private BluntWeapon _bluntWeapon = new BluntWeapon();
 
         internal Weapon()
         {
             _weapons = new List<IInventoryItem>();
             _weapons.Add(_bladeWeapons);
+            _weapons.Add(_bluntWeapon);
         }
 
         public void DisplayName()
@@ -57,12 +59,22 @@ namespace Composite
             if (weaponToAdd.GetType().IsSubclassOf(typeof(BladeWeapon)))
             {
                 _bladeWeapons.AddBladeWeapon(weaponToAdd);
+            }else if (weaponToAdd.GetType().IsSubclassOf(typeof(BluntWeapon)))
+            {
+                _bluntWeapon.AddBluntWeapon(weaponToAdd);
             }
         }
 
         internal void RemoveWeapon(IInventoryItem weaponToDelete)
         {
-            _weapons.Remove(weaponToDelete);
+            if (weaponToDelete.GetType().IsSubclassOf(typeof(BladeWeapon)))
+            {
+                _bladeWeapons.RemoveBladeWeapon(weaponToDelete);
+            }
+            else if (weaponToDelete.GetType().IsSubclassOf(typeof(BluntWeapon)))
+            {
+                _bluntWeapon.RemoveBluntWeapon(weaponToDelete);
+            }
         }
 
         public void Load(List<IInventoryItem> itemsList)
